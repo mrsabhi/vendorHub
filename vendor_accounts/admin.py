15c884216b098f8e714b39de533165209b0b1379
abhishek <mrsabhi7@gmail.com>
2023-09-28 16:06:46 +0530

    add vendor photo

--- vendor_accounts/admin.py
+++ vendor_accounts/admin.py
@@ -8,7 +8,7 @@ from vendor_accounts.models import VendorAccounts
 class VendorAccountsAdmin(admin.ModelAdmin):
     list_display = [
         "vendor_name", "vendor_email", "vendor_phone_number", "shop_name",
-        "state", "address", "is_vendor", "gstin", "pin_code",
+        "state", "address", "is_vendor", "gstin", "vendor_photo", "pin_code",
         "created_at", "updated_at",
     ]
     search_fields = ["email", "vendor_name"]
